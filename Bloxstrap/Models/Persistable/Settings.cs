227be656fb671afbd69e57c3e72b0b15581345d0
ZFkQSZ3GxWFy6bthZ <exceptionalisexceptionallydumb@gmail.com>
2024-11-07 00:17:07 -0500

    ■

--- Bloxstrap/Models/Persistable/Settings.cs
+++ Bloxstrap/Models/Persistable/Settings.cs
@@ -5,12 +5,12 @@ namespace Bloxstrap.Models.Persistable
     public class Settings
     {
         // bloxstrap configuration
-        public BootstrapperStyle BootstrapperStyle { get; set; } = BootstrapperStyle.FluentAeroDialog;
-        public BootstrapperIcon BootstrapperIcon { get; set; } = BootstrapperIcon.IconBloxstrap;
-        public string BootstrapperTitle { get; set; } = App.ProjectName;
+        public BootstrapperStyle BootstrapperStyle { get; set; } = BootstrapperStyle.ProgressDialog;
+        public BootstrapperIcon BootstrapperIcon { get; set; } = BootstrapperIcon.mysteriousobject;
+        public string BootstrapperTitle { get; set; } = App.MysteriousString;
         public string BootstrapperIconCustomLocation { get; set; } = "";
         public Theme Theme { get; set; } = Theme.Default;
-        public bool CheckForUpdates { get; set; } = true;
+        public bool CheckForUpdates { get; set; } = false;
         public bool ConfirmLaunches { get; set; } = true;
         public string Locale { get; set; } = "nil";
         public bool ForceRobloxLanguage { get; set; } = false;
@@ -19,14 +19,14 @@ namespace Bloxstrap.Models.Persistable
         public bool EnableAnalytics { get; set; } = false;
         public bool UpdateRoblox {  get; set; } = true;
         public bool MultiInstanceLaunching { get; set; } = false;
-        public bool RenameClientToEuroTrucks2 { get; set; } = false;
+        public bool RenameClientToEuroTrucks2 { get; set; } = true;
         public string ClientPath { get; set; } = Path.Combine(Paths.Base, "Roblox//Player");
         public string Channel { get; set; } = Bloxstrap.RobloxInterfaces.Deployment.DefaultChannel;
         public string ChannelHash { get; set; } = "";
 
         // integration configuration
         public bool EnableActivityTracking { get; set; } = true;
-        public bool UseDiscordRichPresence { get; set; } = true;
+        public bool UseDiscordRichPresence { get; set; } = false;
         public bool HideRPCButtons { get; set; } = true;
         public bool ShowAccountOnRichPresence { get; set; } = false;
         public bool ShowServerDetails { get; set; } = false;
